8515b72234b6499d33bcf38f4fc7e7c67593c7c3
Aditya Tri Prasetyo <johndoe@example.com>
2021-09-19 10:23:01 +0700

    Tugas Praktikum

---
+++ ATambahB.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ATambahB {
+    public static void main(String[] args){
+        Scanner bebek = new Scanner(System.in);
+        int A, B;
+        A = bebek.nextInt();
+        B = bebek.nextInt();
+
+        int Total;
+        Total = A + B;
+        
+        System.out.println(Total);
+        bebek.close();
+    }
+}
---
+++ BurungBeo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class BurungBeo {
+    public static void main(String[] args){
+        Scanner Beo = new Scanner(System.in);
+        String beo;
+        beo = Beo.nextLine();
+
+        System.out.println(beo);
+        Beo.close();
+    }
+}
